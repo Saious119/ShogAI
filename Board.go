49473de8d0c2c836a7a1ed35eead3858017262d1
Hunter Bashaw <hunter@blivet.sh>
2020-05-04 16:39:44 -0400

    Initialize Pieces

--- Board.go
+++ Board.go
@@ -26,3 +26,69 @@ func PrintBoard(board [][]string) { //prints the given board
 		fmt.Println()
 	}
 }
+
+func InitPieces() []Pair {
+	var pieces []Pair
+	pair := Pair{0, 0, "L1"}
+	pieces = append(pieces, pair)
+	pair = Pair{8, 0, "L1"}
+	pieces = append(pieces, pair)
+	pair = Pair{0, 8, "L2"}
+	pieces = append(pieces, pair)
+	pair = Pair{8, 8, "L2"}
+	pieces = append(pieces, pair)
+
+	pair = Pair{1, 0, "N1"}
+	pieces = append(pieces, pair)
+	pair = Pair{7, 0, "N1"}
+	pieces = append(pieces, pair)
+	pair = Pair{1, 8, "N2"}
+	pieces = append(pieces, pair)
+	pair = Pair{7, 8, "N2"}
+	pieces = append(pieces, pair)
+
+	pair = Pair{2, 0, "S1"}
+	pieces = append(pieces, pair)
+	pair = Pair{6, 0, "S1"}
+	pieces = append(pieces, pair)
+	pair = Pair{2, 8, "S2"}
+	pieces = append(pieces, pair)
+	pair = Pair{6, 8, "S2"}
+	pieces = append(pieces, pair)
+
+	pair = Pair{3, 0, "G1"}
+	pieces = append(pieces, pair)
+	pair = Pair{5, 0, "G1"}
+	pieces = append(pieces, pair)
+	pair = Pair{3, 8, "G2"}
+	pieces = append(pieces, pair)
+	pair = Pair{5, 8, "G2"}
+	pieces = append(pieces, pair)
+
+	pair = Pair{4, 0, "K1"}
+	pieces = append(pieces, pair)
+	pair = Pair{4, 8, "K2"}
+	pieces = append(pieces, pair)
+
+	pair = Pair{1, 1, "R1"}
+	pieces = append(pieces, pair)
+	pair = Pair{1, 7, "R2"}
+	pieces = append(pieces, pair)
+
+	pair = Pair{7, 1, "B1"}
+	pieces = append(pieces, pair)
+	pair = Pair{7, 7, "B2"}
+	pieces = append(pieces, pair)
+
+	for i := 0; i < 9; i++ {
+		pair = Pair{i, 2, "P1"}
+		pieces = append(pieces, pair)
+	}
+
+	for i := 0; i < 9; i++ {
+		pair = Pair{i, 6, "P2"}
+		pieces = append(pieces, pair)
+	}
+
+	return pieces
+}
--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	board := InitBoard()
 	PrintBoard(board)
+	state := ShogiState{
+		board:  board,
+		pieces: InitPieces(),
+		parent: nil,
+	}
+
+	m, err := MiniMax(state, 1, 5)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(m)
+
 }
