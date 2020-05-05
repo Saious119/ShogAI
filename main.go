ba26bb1ee4d37e83a0c0d7c61ef01c7ab48e4d98
Hunter Bashaw <hunter@blivet.sh>
2020-05-05 16:35:05 -0400

    MOAR AUTOMATION

--- main.go
+++ main.go
@@ -3,20 +3,60 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 	"time"
 )
 
+func (state ShogiState) updateCoords(coords []string) ShogiState {
+	newState := duplicate(state)
+	currX, err := strconv.Atoi(coords[0])
+	if err != nil {
+		panic(err)
+	}
+	currY, err := strconv.Atoi(coords[1])
+	if err != nil {
+		panic(err)
+	}
+	finalX, err := strconv.Atoi(coords[2])
+	if err != nil {
+		panic(err)
+	}
+	finalY, err := strconv.Atoi(coords[3])
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(state.pieces); i++ {
+		if state.pieces[i].x == currX && state.pieces[i].y == currY {
+			piece := newState.pieces[i].name
+			newState.board[newState.pieces[i].y][newState.pieces[i].x] = "O"
+			newState.board[finalY][finalX] = piece //update board
+			newState.pieces[i].x = finalX          //update piece
+			newState.pieces[i].y = finalY
+			if !strings.Contains(piece, "+") {
+				if !strings.Contains(piece, "K") {
+					if CheckPromotion(finalY, newState.pieces[i].name) {
+						newState.board[finalY][finalX] = piece + "+"
+						newState.pieces[i].name = piece + "+"
+					}
+				}
+			}
+			break
+		}
+	}
+
+	return newState
+}
+
 func main() {
 	board := InitBoard()
-	// PrintBoard(board)
 	state := ShogiState{
 		board:  board,
 		pieces: InitPieces(),
 		parent: nil,
 	}
-	// fmt.Println(state)
 
-	// bFile, err := os.Open("./NodeScriptShogAI/board.txt")
 	for {
 		fmt.Println(state)
 		m, err := MiniMax(state, 2, 1)
@@ -26,6 +66,13 @@ func main() {
 		fmt.Println(m)
 		ioutil.WriteFile("./NodeScriptShogAI/move.txt", []byte(m.String()), 0644)
 		time.Sleep(30 * time.Second)
+
+		data, err := ioutil.ReadFile("./NodeScriptShogAI/board.txt")
+		if err != nil {
+			panic(err)
+		}
+		coords := strings.Split(string(data), " ")
+		state = state.updateCoords(coords)
 	}
 
 }
